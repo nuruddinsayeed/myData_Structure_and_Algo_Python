00b6b94c31629803d5b912b82fe47c46e16f6c7d
Sayed <nuruddinsayeed@gmail.com>
2021-10-30 20:13:17 +0600

    tree depth sum probmelm solved

--- sample_test.py
+++ sample_test.py
@@ -1,6 +1,8 @@
-sequence = [1,2,3,4,5,6,7,8]
+sequence = [1, 2, 3, 4, 5, 6, 7, 8]
 
 doubled = [(lambda x: x*2)(x) for x in sequence]
+doubled2 = [x*2 for x in sequence]
 
 print(doubled)
+print(doubled2)
 print(sequence[:3])
